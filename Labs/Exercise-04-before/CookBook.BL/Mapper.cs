886adba65e74da680169456698475bd77f8049fc
Michal Mrnustik <michal.mrnustik@outlook.com>
2019-03-27 21:04:02 +0100

    Add mapper

---
+++ Labs/Exercise-04-before/CookBook.BL/Mapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CookBook.BL.Models;
+using CookBook.DAL.Entities;
+
+namespace CookBook.BL
+{
+    class Mapper
+    {
+        public static IngredientDetailModel MapIngredientEntityToDetailModel(IngredientEntity entity)
+        {
+            return new IngredientDetailModel
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Description = entity.Description
+            };
+        }
+
+        public static IngredientListModel MapIngredientEntityToListModel(IngredientEntity entity)
+        {
+            return new IngredientListModel
+            {
+                Id = entity.Id,
+                Name = entity.Name
+            };
+        }
+
+        public static IngredientEntity MapIngredientModelToEntity(IngredientDetailModel model)
+        {
+            return new IngredientEntity()
+            {
+                Id = model.Id,
+                Name = model.Name
+            };
+        }
+    }
+}
